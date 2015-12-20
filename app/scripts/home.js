091836e6bb64229dd8e6970176caa62aba7e9ed0
lock0 <xiaoxiao95888@163.com>
2015-12-20 18:01:11 +0800

    ben

---
+++ app/scripts/home.js
@@ -0,0 +1,30 @@
+var Home = {
+  viewModel: {
+    cardTypes: ko.observableArray(),
+    employees: ko.observableArray(),
+    wechatuser: {
+      Id: ko.observable(),
+      NickName: ko.observable(),
+      Gender: ko.observable(),
+      Language: ko.observable(),
+      City: ko.observable(),
+      Province: ko.observable(),
+      Country: ko.observable(),
+      Headimgurl: ko.observable(),
+    }
+  }
+};
+$(function () {
+  ko.applyBindings(Home);
+  $.get("http://MangoCardsApi.local/api/CardType/", function (data) {
+    ko.mapping.fromJS(data, {}, Home.viewModel.cardTypes);
+    $.get("http://MangoCardsApi.local/api/Employee/", function (employees) {
+      ko.mapping.fromJS(employees, {}, Home.viewModel.employees);
+      $.get("http://MangoCardsApi.local/api/WeChatUser/", function (wechatuser) {
+        if (wechatuser != null) {
+          ko.mapping.fromJS(wechatuser, {}, Home.viewModel.wechatuser);
+        }
+      });
+    });
+  });
+});
