24de6f0c2f09b893352309d4ac411114b3a331af
Ben <xiaoxiao95888@163.com>
2015-12-22 14:34:25 +0800

    data-filter问题

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -17,11 +17,11 @@ var Home = {
   console.log('home')
   ko.applyBindings(Home);
 
-  $.get("http://MangoCardsApi.local/api/CardType/", function (data) {
+  $.get("http://api.card.mangoeasy.com/api/CardType/", function (data) {
     ko.mapping.fromJS(data, {}, Home.viewModel.cardTypes);
-    $.get("http://MangoCardsApi.local/api/Employee/", function (employees) {
+    $.get("http://api.card.mangoeasy.com/api/Employee/", function (employees) {
       ko.mapping.fromJS(employees, {}, Home.viewModel.employees);
-      $.get("http://MangoCardsApi.local/api/WeChatUser/", function (wechatuser) {
+      $.get("http://api.card.mangoeasy.com/api/WeChatUser/", function (wechatuser) {
         if (wechatuser != null) {
           ko.mapping.fromJS(wechatuser, {}, Home.viewModel.wechatuser);
         }
