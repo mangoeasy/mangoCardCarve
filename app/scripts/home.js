f32a7d14ad2ed6b20bff845b1948723be548fa21
iamike <dai.mike@gmail.com>
2016-01-01 03:10:40 +0800

    update

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -14,24 +14,63 @@ var Home = {
     }
   }
 };
+
+// Converts canvas to an image
+function convertCanvasToImage(canvas) {
+	var image = new Image();
+	image.src = canvas.toDataURL("image/png");
+	return image;
+}
+// Converts image to canvas; returns new canvas element
+function convertImageToCanvas(image) {
+	var canvas = document.createElement("canvas");
+	canvas.width = image.width;
+	canvas.height = image.height;
+	canvas.getContext("2d").drawImage(image, 0, 0);
+
+	return canvas;
+}
+
 ko.bindingHandlers.qrbind = {
   init: function(element, valueAccessor, allBindingsAccessor, viewModel) {
     // This will be called when the binding is first applied to an element
     // Set up any initial state, event handlers, etc. here
   },
-  update: function (element, valueAccessor) {
+  update: function(element, valueAccessor) {
     var data = ko.unwrap(valueAccessor());
     var url = window.location.href + "demo.html?id=" + data;
-    $(element).qrcode(url);
+    var $item = $(element).find('.work-canvas-link');
+
+    $(element).find('.work-canvas-link').qrcode(url).find('canvas').hide();
+
+    $item.attr('href', "#").on('click', function(e) {
+      e.preventDefault();
+      var thisItem = $(this);
+      var thisCanvasImage = convertCanvasToImage(thisItem.find('canvas')[0]);
+      var thisHtmlString = '<div class="mfp-figure"><figure>'+$(thisCanvasImage).addClass('mfp-img').prop('outerHTML')+'</figure></div>';
+
+
+
+      $.magnificPopup.open({
+        items: {
+          src: thisHtmlString ,// can be a HTML string, jQuery object, or CSS selector
+          type: 'inline'
+        }
+      });
+
+    });
+
+
+
   }
 };
 ko.applyBindings(Home);
-$.get("http://api.card.mangoeasy.com/api/CardType/", function (data) {
+$.get("http://api.card.mangoeasy.com/api/CardType/", function(data) {
   ko.mapping.fromJS(data, {}, Home.viewModel.cardTypes);
-  $.get("http://api.card.mangoeasy.com/api/Employee/", function (employees) {
+  $.get("http://api.card.mangoeasy.com/api/Employee/", function(employees) {
     ko.mapping.fromJS(employees, {}, Home.viewModel.employees);
     initWorkFilter();
-    $.get("http://api.card.mangoeasy.com/api/WeChatUser/", function (wechatuser) {
+    $.get("http://api.card.mangoeasy.com/api/WeChatUser/", function(wechatuser) {
       if (wechatuser != null) {
         ko.mapping.fromJS(wechatuser, {}, Home.viewModel.wechatuser);
       }
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -328,6 +328,7 @@
     });
     $(".lightbox").magnificPopup();
 
+
   }
 
 
